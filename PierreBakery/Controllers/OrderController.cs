2ec2de00124167dc84bf14b9a86f80c4a0fb22e3
gramsower <110431307+gramsower@users.noreply.github.com>
2022-12-12 16:40:20 -0800

    WIP troubleshooting route templates

--- PierreBakery/Controllers/OrderController.cs
+++ PierreBakery/Controllers/OrderController.cs
@@ -1,35 +1,35 @@
-using Microsoft.AspNetCore.Mvc;
-using Bakery.Models;
-using System.Collections.Generic;
+// using Microsoft.AspNetCore.Mvc;
+// using Bakery.Models;
+// using System.Collections.Generic;
 
-namespace Bakery.Controllers
-{
-  public class OrderController : Controller
-  {
+// namespace Bakery.Controllers
+// {
+//   public class OrderController : Controller
+//   {
 
-    [HttpGet("/vendors/{vendorId}/orders/new")]
-    public ActionResult New(int vendorId)
-    {
-      Vendor vendor = Vendor.Find(vendorId);
-      return View(vendor);
-    }
+//     [HttpGet("/vendors/{vendorId}/orders/new")]
+//     public ActionResult New(int vendorId)
+//     {
+//       Vendor vendor = Vendor.Find(vendorId);
+//       return View(vendor);
+//     }
 
-    [HttpGet("/vendor/{vendorId}/orders/{ordId}")]
-    public ActionResult Show(int vendorId, int ordId)
-    {
-      Order order = Order.Find(ordId);
-      Vendor vendor = Vendor.Find(vendorId);
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      model.Add("order", order);
-      model.Add("vendor", vendor);
-      return View(model);
-    }
+//     [HttpGet("/vendors/{vendorId}/orders/{ordId}")]
+//     public ActionResult Show(int vendorId, int ordId)
+//     {
+//       Order order = Order.Find(ordId);
+//       Vendor vendor = Vendor.Find(vendorId);
+//       Dictionary<string, object> model = new Dictionary<string, object>();
+//       model.Add("order", order);
+//       model.Add("vendor", vendor);
+//       return View(model);
+//     }
 
-    [HttpPost("/orders/delete")]
-    public ActionResult DeleteAll()
-    {
-      Order.ClearAll();
-      return View();
-    }
-  }
-}
+//     [HttpPost("/orders/delete")]
+//     public ActionResult DeleteAll()
+//     {
+//       Order.ClearAll();
+//       return View();
+//     }
+//   }
+// }
